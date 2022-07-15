ef4aae67efa837991a1a7356f41757b33b1036a1
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-07-15 18:07:51 +0700

    feat: add about activity

---
+++ app/src/main/java/com/mirzaq/androidnewbie/AboutActivity.kt
@@ -0,0 +1,15 @@
+package com.mirzaq.androidnewbie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        supportActionBar?.title = "About me"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+}
--- app/src/main/java/com/mirzaq/androidnewbie/MainActivity.kt
+++ app/src/main/java/com/mirzaq/androidnewbie/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.mirzaq.androidnewbie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -24,5 +27,24 @@ class MainActivity : AppCompatActivity() {
     private fun showRecyclerList() {
         rvScientist.layoutManager = LinearLayoutManager(this)
         rvScientist.adapter = ListScientistAdapter(list)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.about, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.btn_about -> {
+                val moveIntent = Intent(this@MainActivity, AboutActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
